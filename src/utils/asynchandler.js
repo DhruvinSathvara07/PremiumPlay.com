997d5d73791cd2e0c125377714eac023677557b3
Dhruvin <sathvaradhruvin05@gmail.com>
2025-10-18 15:55:39 +0530

    changes

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,14 +1,14 @@
-export const asyncHandler = (fn) => {
-    return async (req, res, next) => {
-        try {
-            await fn(req, res, next);
-        } catch (error) {
-            console.error("Async Handler Error:", error.message);
-            res.status(500).json({
-                success: false,
-                message: "Internal Server Error",
-                error: error.message,
-            });
-        }
-    };
-};
+// export const asyncHandler = (fn) => {
+//     return async (req, res, next) => {
+//         try {
+//             await fn(req, res, next);
+//         } catch (error) {
+//             console.error("Async Handler Error:", error.message);
+//             res.status(500).json({
+//                 success: false,
+//                 message: "Internal Server Error",
+//                 error: error.message,
+//             });
+//         }
+//     };
+// };
