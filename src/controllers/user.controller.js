d5382eb7aca3427bd18c07b8a91688a097a1ee50
Dhruvin <sathvaradhruvin05@gmail.com>
2025-10-18 15:45:07 +0530

    Changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import ApiError from "../utils/apiError.js";
 import { User } from "../models/user.model.js";
-import { uploadOnCloudinary } from "../utils/cloudniary.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import ApiResponse from "../utils/apiResponse.js";
 import fs from "fs";
 // import fs from "fs";
